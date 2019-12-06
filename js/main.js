288e396b4849e372ffe0d937b46a23a6245d8888
Singh <i_singh94252@F1000-MAC-08.fc.ca>
2019-12-05 21:19:13 -0500

    done with some ajax stuff

---
+++ js/main.js
@@ -0,0 +1,99 @@
+//header
+
+
+
+
+
+TweenMax.to("#green_apart", 0.001,{scale:0});
+function show(){
+    TweenMax.to("#green_apart", 0.5,{scale:1, delay:0});
+}
+document.querySelector("#pie_chart").addEventListener("mouseover", show)
+TweenMax.to("#factors", 0.001,{opacity:0});
+function show1(){
+    TweenMax.to("#factors", 3,{opacity:1, delay:0});
+}
+document.querySelector("#pie_chart").addEventListener("mouseover", show1)
+
+TweenMax.to("#red", 0.001,{scale:0});
+function show2(){
+    TweenMax.to("#red", 1,{scale:1, delay:0});
+}
+document.querySelector("#Header_text").addEventListener("mouseover", show2)
+
+TweenMax.to("#yellow", 0.001,{scale:0});
+function show3(){
+    TweenMax.to("#yellow", 1,{scale:1, delay:0.1});
+}
+document.querySelector("#Header_text").addEventListener("mouseover", show3)
+
+TweenMax.to("#light_green", 0.001,{scale:0});
+function show4(){
+    TweenMax.to("#light_green", 1,{scale:1, delay:0.2});
+}
+document.querySelector("#Header_text").addEventListener("mouseover", show4)
+
+TweenMax.to("#green_path", 0.001,{scale:0});
+function show5(){
+    TweenMax.to("#green_path", 1,{scale:1, delay:0.3});
+}
+document.querySelector("#Header_text").addEventListener("mouseover", show5)
+
+TweenMax.to("#black_tick", 0.001,{opacity:0});
+function show6(){
+    TweenMax.to("#black_tick", 1,{opacity:1, delay: 0.5});
+}
+document.querySelector("#Header_text").addEventListener("mouseover", show6)
+
+
+TweenMax.to("#ways_chart", 0.001,{opacity:0});
+function show7(){
+    TweenMax.to("#ways_chart", 3,{opacity:1, delay:0});
+}
+document.querySelector("#ways_flow_chart").addEventListener("mouseover", show7)
+
+
+
+TweenMax.to("#pie_chart", 0.001,{right:0});
+function show8(){
+    TweenMax.to("#pie_chart", 3,{right:1, delay:0});
+}
+document.querySelector("#pie_chart").addEventListener("mouseover", show8)
+
+
+
+TweenMax.to("#bad", 0.001,{opacity:0});
+function show9(){
+    TweenMax.to("#bad", 0.5,{opacity:1, delay:0});
+}
+document.querySelector("#Header_text").addEventListener("mouseover", show9)
+
+TweenMax.to("#fair", 0.001,{opacity:0});
+function show10(){
+    TweenMax.to("#fair", 0.5,{opacity:1, delay:0});
+}
+document.querySelector("#Header_text").addEventListener("mouseover", show10)
+
+TweenMax.to("#good", 0.001,{opacity:0});
+function show11(){
+    TweenMax.to("#good", 0.5,{opacity:1, delay:0});
+}
+document.querySelector("#Header_text").addEventListener("mouseover", show11)
+
+TweenMax.to("#excellent", 0.001,{opacity:0});
+function show12(){
+    TweenMax.to("#excellent", 0.5,{opacity:1, delay:0});
+}
+document.querySelector("#Header_text").addEventListener("mouseover", show12)
+
+TweenMax.to("#text700", 0.001,{scale:0});``
+function show13(){
+    TweenMax.to("#text700", 1,{scale:1, delay:0});
+}
+document.querySelector("#Header_text").addEventListener("mouseover", show13)
+
+TweenMax.to("#Header_text", 0.001,{opacity:1});
+function show14(){
+    TweenMax.to("#Header_text", 1,{opacity:2, delay:0});
+}
+document.querySelector("#Header_text").addEventListener("mouseover", show14)
